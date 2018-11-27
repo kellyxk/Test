d8e13255c5de4d68ff2093e5c2c7d5d9f24ff84e
kellyxk <xk565002879@163.com>
2018-11-27 17:26:44 +0800

    success drag

--- drag.js
+++ drag.js
@@ -2,16 +2,30 @@
 function drag(id){
 	var obj=document.getElementById(id);
 	var disX=0;
-	var dixY=0;
+	var disY=0;
 	obj.onmousedown=function(ev){
 		disX=ev.pageX-obj.offsetLeft;
 		disY=ev.pageY-obj.offsetTop;
 		document.onmousemove=function(ev){
-			obj.style.left=ev.pageX-disX+'px';
-			obj.style.top=ev.pageY-disY+'px';
-			}
+			/*github老师用的left和top不能拖拽，我用的marginLeft
+			和marginTop解决了问题*/
+			if(obj.style.marginLeft<0){
+				obj.style.marginLeft==0
+				}
+			if(obj.style.marginTop<0){
+				obj.style.marginTop==0
+				}	
+				
+			obj.style.marginLeft=ev.pageX-disX+'px';
+			obj.style.marginTop=ev.pageY-disY+'px';
+			};
+		document.onmouseup=function(){
+			document.onmousemove=null;
+			document.onmouseup=null;
+			};	
 
 		}
+		return false;
 	}
 	
 	
